2aeb4ffdf701e7386e51fa46b8bcf94da82fa714
qwark97 <marcin.plata1@gmail.com>
2023-03-23 22:57:51 +0100

    slice to list stuff

---
+++ src/slice_list/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	myList := [2]int{1, 2}
+	mySlice := []int{1, 2}
+
+	fmt.Printf("list: %v(%T)\n\n", myList, myList)
+	fmt.Printf("slice: %v(%T)\n\n", mySlice, mySlice)
+
+	// list to slice
+	myNewSlice := myList[:]
+	fmt.Printf("new slice: %v(%T)\n\n", myNewSlice, myNewSlice)
+
+	// slice to list (hack)
+	myNewListHack := *(*[2]int)(mySlice)
+	fmt.Printf("new slice (hack): %v(%T)\n\n", myNewListHack, myNewListHack)
+
+	// slice to list (NEW)
+	myNewList := [2]int(mySlice)
+	fmt.Printf("new slice: %v(%T)\n\n", myNewList, myNewList)
+
+}
